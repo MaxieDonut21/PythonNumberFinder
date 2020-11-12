0226109cf11dba1ddb418770a1da66c36c6702d0
MaxieDonut21 <52567883+MaxieDonut21@users.noreply.github.com>
2020-11-13 10:26:19 +1100

    Add files via upload

---
+++ number finder.py
@@ -0,0 +1,33 @@
+import time
+import random
+F = 0
+
+number = int(input('What number do you want me to find? '))
+if number <= 100 and number > 0:
+    print('Okay, searching now')
+    time.sleep(.4)
+    print('Initiating hacks')
+    time.sleep(.4)
+    print('Database found')
+    time.sleep(.4)
+    print('Attempting login')
+    time.sleep(.4)
+    print('Login successful')
+    time.sleep(.4)
+    print('Searching for number')
+
+
+    while F!= number:
+        F = random.randint(0,100)
+    if F == number:
+        print(number)
+        print('Great, I hacked into the database')
+        for i in range(0,1):
+            print('Hack success!')
+
+
+elif True:
+    print('Oh no, number too chunky!')
+
+
+
